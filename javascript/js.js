90fce23130ee4e89de574ab79125b10ed6434a37
Jessica Tran <traan.jessicaa@gmail.com>
2017-11-13 21:34:42 -0800

    local storage and JSON for join name

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,28 @@
+var users;
+if (localStorage.getItem('users') === null) {
+  console.log('empty: ' + JSON.stringify(localStorage));
+  users = [];
+} else {
+  users = JSON.parse(localStorage.getItem('users'));
+  console.log('hi, not empty:' + JSON.stringify(localStorage));
+}
+
+alert('users: ' + JSON.stringify(users));
+
+function getName() {
+  var first = document.getElementById('userFirstName').value;
+  var last = document.getElementById('userLastName').value;
+  var fullName = first + " " + last;
+  var person = {"name": fullName};
+  console.log(JSON.stringify(person));
+  alert('pushing: ' + JSON.stringify(person));
+  alert('users: ' + JSON.stringify(users));
+  users.push(person);
+  localStorage.setItem('users', JSON.stringify(users));
+  console.log('localStorage: ' + localStorage);
+  window.location.href = './join_email.html';
+}
+
 $('#back-join').click(function(){
   window.history.back();
 });
